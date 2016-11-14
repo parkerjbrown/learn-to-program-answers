a2c8c9a32a33003f479e1359ab8ed7c116d3b1b0
Parker Brown <parkerjbrown@users.noreply.github.com>
2016-11-14 10:29:29 -0500

    Update ch05.01.rb

--- ch05.01.rb
+++ ch05.01.rb
@@ -5,4 +5,4 @@ middle_name = gets.chomp
 puts "And your last name?"
 last_name = gets.chomp
 name_length = first_name.length + middle_name.length + last_name.length
-puts "Well then, it's nice to meet you " + first_name + " " + last_name + "! Did you know that there are " + name_length.to_s + " letters in your name? Also, did you know that I probably have autism?"
+puts "Well then, it's nice to meet you " + first_name + " " + last_name + "! Did you know that there are " + name_length.to_s + " letters in your name?"
